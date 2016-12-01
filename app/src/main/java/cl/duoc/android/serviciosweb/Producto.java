e40860a2cd47094a0ea2fcf7b8102630059f2bec
Oscar Rojas <oesrr69@gmail.com>
2016-12-01 15:14:08 -0300

    Initial commit

---
+++ app/src/main/java/cl/duoc/android/serviciosweb/Producto.java
@@ -0,0 +1,34 @@
+package cl.duoc.android.serviciosweb;
+
+public class Producto {
+
+    private String id ;
+    private String nombre;
+
+    public Producto() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id='" + id + '\'' +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+}
